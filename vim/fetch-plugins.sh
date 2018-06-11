a4a22fcca06e0f5d7033809c0c017a856141a454
jackam <dev@thewhiteurchin.com>
2018-06-11 13:28:02 -1000

    Made a script to install all plugins

---
+++ vim/fetch-plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/scrooloose/nerdtree.git ./bundle/nerdtree
+
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ./bundle/ctrlp
+
+git clone https://github.com/vim-airline/vim-airline.git ./bundle/vim-airline
+
+git clone https://github.com/vim-airline/vim-airline-themes.git ./bundle/vim-airline-themes
+
+git clone https://github.com/bling/vim-bufferline.git ./bundle/vim-bufferline
+
+git clone https://github.com/airblade/vim-gitgutter.git ./bundle/vim-gitgutter 
+
+
